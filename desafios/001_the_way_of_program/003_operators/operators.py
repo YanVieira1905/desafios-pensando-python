bd58bd5c9eba5687b1cd507c7cfd4674ed6600a9
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-09-19 10:27:04 -0300

    Add desafios operators

---
+++ desafios/001_the_way_of_program/003_operators/operators.py
@@ -0,0 +1,27 @@
+def add(first_number, second_number):
+  # Retorne a soma dos parâmetros first_number e second_number
+  pass
+
+def sub(first_number, second_number):
+  # Retorne a subtração dos parâmetros first_number e second_number
+  pass
+
+def mult(first_number, second_number):
+  # Retorne a multiplicação dos parâmetros first_number e second_number
+  pass
+
+def div(first_number, second_number):
+  # Retorne a divisão dos parâmetros first_number e second_number
+  pass
+
+def expo(first_number, second_number):
+  # Retorne a exponenciação dos parâmetros first_number e second_number
+  pass
+
+def remai(first_number, second_number):
+  # Retorne o resto da divisão dos parâmetros first_number e second_number
+  pass
+
+def quoti_remai(first_number, second_number):
+  # Retorne o quociente e o resto da divisão, nesta exata ordem, dos parâmetros first_number e second_number
+  pass
