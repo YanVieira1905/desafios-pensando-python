cc9d62395cb1959fdcb503151a255899d544631f
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-09-19 10:32:29 -0300

    Correção desafios/001_the_way_of_program/003_operators/operators.py

--- desafios/001_the_way_of_program/003_operators/operators.py
+++ desafios/001_the_way_of_program/003_operators/operators.py
@@ -1,22 +1,22 @@
 def add(first_number, second_number):
   # Retorne a soma dos parâmetros first_number e second_number
-  pass
+  return first_number + second_number
 
 def sub(first_number, second_number):
   # Retorne a subtração dos parâmetros first_number e second_number
-  pass
+  return first_number - second_number
 
 def mult(first_number, second_number):
   # Retorne a multiplicação dos parâmetros first_number e second_number
-  pass
+  return first_number * second_number
 
 def div(first_number, second_number):
   # Retorne a divisão dos parâmetros first_number e second_number
-  pass
+  return first_number / second_number
 
 def expo(first_number, second_number):
   # Retorne a exponenciação dos parâmetros first_number e second_number
-  pass
+  return first_number ** second_number
 
 def remai(first_number, second_number):
   # Retorne o resto da divisão dos parâmetros first_number e second_number
